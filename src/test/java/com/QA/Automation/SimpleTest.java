0adae39e408ba18ccf7e6847da3381fcf11fdb92
Vadim Fazliev <vfa@tassta.com>
2022-05-26 22:08:00 +0300

    Gradle Gjunit5

--- src/test/java/com/QA/Automation/SimpleTest.java
+++ src/test/java/com/QA/Automation/SimpleTest.java
@@ -1,4 +1,47 @@
 package com.QA.Automation;
 
+import org.junit.jupiter.api.*;
+
 public class SimpleTest {
+    @BeforeAll
+    static void initDB() {
+        System.out.println("### BeforeAll");
+    }
+
+    @AfterAll
+    static void cleanDB() {
+        System.out.println("### AfterAll");
+    }
+
+    @BeforeEach
+    void openYaPage() {
+        System.out.println("###         BeforeEach");
+        // Selenide.open("https://ya.ru");
+    }
+
+    @AfterEach
+    void closeYaPage() {
+        System.out.println("###         AfterEach");
+        // WebDriverRunner.closeWindow();
+
+    }
+
+    @Test
+    void assertTest1() {
+        System.out.println("###     Test1");
+        //...поиск в новостях
+    }
+
+    @Test
+    void assertTest2() {
+        System.out.println("###     Test2");
+        //...поиск в картинках
+    }
+
+    @Test
+    void assertTest3() {
+        System.out.println("###     Test3");
+        //...поиск в чем-то
+    }
 }
+
