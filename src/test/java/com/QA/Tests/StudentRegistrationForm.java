472c440dedfcecdeea739df3c0a2d98d3ee3931f
VadimFazlyev <FazlyevVR@yandex.ru>
2022-06-03 17:53:48 +0300

    Home work with new fields

--- src/test/java/com/QA/Tests/StudentRegistrationForm.java
+++ src/test/java/com/QA/Tests/StudentRegistrationForm.java
@@ -3,6 +3,7 @@ package com.QA.Tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -33,8 +34,14 @@ public class StudentRegistrationForm {
         $(".react-datepicker__year-select").selectOption(87);//how click on my year - 1987?
         $(".react-datepicker__year-select").click();
         $(".react-datepicker__day--012").click(); //day
+        $(".subjects-auto-complete__value-container").click(); //container
+        $("#subjectsInput").setValue("English").pressEnter();
+        $("#hobbies-checkbox-1").parent().click(); //hobbies  parent()???
+        //$(".form-file-label").click(); //choose file
+        $("#currentAddress").setValue("City Kydrovo, Street Angliiskaya 2"); // Current Address
 
-//        $("[id=subjectsContainer]").click(); //container
+
+
 
 
     }
